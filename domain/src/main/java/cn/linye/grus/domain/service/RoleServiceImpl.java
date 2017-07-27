51c05b953ddb41240170f0396836e7023203d9f7
tianzhonghai <tianzhonghai@ymatou.com>
2017-07-27 17:15:55 +0800

    角色管理

--- domain/src/main/java/cn/linye/grus/domain/service/RoleServiceImpl.java
+++ domain/src/main/java/cn/linye/grus/domain/service/RoleServiceImpl.java
@@ -4,10 +4,17 @@ import cn.linye.grus.domain.dtos.RoleDto;
 import cn.linye.grus.domain.entity.generated.RoleEntity;
 import cn.linye.grus.domain.entity.generated.RoleEntityExample;
 import cn.linye.grus.domain.repository.RoleRepository;
+import cn.linye.grus.domain.repository.generated.PermissionMapper;
+import cn.linye.grus.domain.repository.generated.RoleMapper;
 import cn.linye.grus.facade.model.admin.req.AddRoleReq;
 import cn.linye.grus.facade.model.admin.req.QueryRolesReq;
 import cn.linye.grus.infrastructure.PagedCollection;
+import cn.linye.grus.infrastructure.exception.BizException;
 import cn.linye.grus.infrastructure.utils.DozerUtils;
+import cn.linye.grus.infrastructure.utils.SpringUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +29,12 @@ public class RoleServiceImpl implements RoleService {
 
     @Autowired
     private RoleRepository roleRepository;
+
+    @Autowired
+    private RoleMapper roleMapper;
+
+    @Autowired
+    private PermissionMapper permissionMapper;
 
     public PagedCollection<RoleDto> queryRoleList(QueryRolesReq queryRolesReq) {
         RoleEntityExample roleEntityExample = new RoleEntityExample();
@@ -40,6 +53,25 @@ public class RoleServiceImpl implements RoleService {
     }
 
     public void addRole(AddRoleReq addRoleReq) {
+        SqlSessionFactoryBean sqlSessionFactoryBean = SpringUtils.getBean(SqlSessionFactoryBean.class);
+        SqlSessionFactory sqlSessionFactory = null;
+        try {
+            sqlSessionFactory = sqlSessionFactoryBean.getObject();
+        } catch (Exception ex) {
+            BizException.throwFail(ex.getMessage(),ex);
+        }
 
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            int userId = roleMapper.insert(null);
+            for (int roleId :addRoleReq.getPermissionIds()) {
+                permissionMapper.insert(null);
+            }
+
+            sqlSession.commit();
+        } catch (Exception ex) {
+            sqlSession.rollback();
+            BizException.throwFail(ex.getMessage(),ex);
+        }
     }
 }
