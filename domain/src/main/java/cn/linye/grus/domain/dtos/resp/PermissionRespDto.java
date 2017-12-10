6d1e0e51f7b973b7778e0031456d04b89eaa6257
tianzhonghai <tianzhonghai@outlook.com>
2017-12-10 19:19:59 +0800

    引入lombok

--- domain/src/main/java/cn/linye/grus/domain/dtos/resp/PermissionRespDto.java
+++ domain/src/main/java/cn/linye/grus/domain/dtos/resp/PermissionRespDto.java
@@ -1,8 +1,14 @@
 package cn.linye.grus.domain.dtos.resp;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by Tim on 2017/7/26.
  */
+@Getter
+@Setter
 public class PermissionRespDto {
     private Integer permissionid;
 
@@ -18,59 +24,4 @@ public class PermissionRespDto {
 
     private Integer parentpermissionid;
 
-    public Integer getPermissionid() {
-        return permissionid;
-    }
-
-    public void setPermissionid(Integer permissionid) {
-        this.permissionid = permissionid;
-    }
-
-    public String getPermissionname() {
-        return permissionname;
-    }
-
-    public void setPermissionname(String permissionname) {
-        this.permissionname = permissionname == null ? null : permissionname.trim();
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description == null ? null : description.trim();
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url == null ? null : url.trim();
-    }
-
-    public Boolean getIsmenu() {
-        return ismenu;
-    }
-
-    public void setIsmenu(Boolean ismenu) {
-        this.ismenu = ismenu;
-    }
-
-    public String getPermissioncode() {
-        return permissioncode;
-    }
-
-    public void setPermissioncode(String permissioncode) {
-        this.permissioncode = permissioncode == null ? null : permissioncode.trim();
-    }
-
-    public Integer getParentpermissionid() {
-        return parentpermissionid;
-    }
-
-    public void setParentpermissionid(Integer parentpermissionid) {
-        this.parentpermissionid = parentpermissionid;
-    }
 }
